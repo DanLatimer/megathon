d8e750e8c0790116bae2aca985a2119878de184f
nathanmyles <nmyles@aio-tv.com>
2014-11-15 12:47:51 -0400

    TASK-6: setup MainMenuScreen
    - add label to TextField
    - add static list of available games
    - open LevelScreen when game is clicked, handing in selected player
    - display message to selected user on LevelScreen
    - MISC: clear buffer on render()
    - MISC: update Viewport on resize()
    - MISC: add convenience method in BaseScreen to get Skin

--- core/src/ca/nakednate/game/MainMenuScreen.java
+++ core/src/ca/nakednate/game/MainMenuScreen.java
@@ -1,9 +1,9 @@
 package ca.nakednate.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class MainMenuScreen extends BaseScreen {
 
@@ -15,16 +15,42 @@ public class MainMenuScreen extends BaseScreen {
     public void show() {
         super.show();
 
+        Label welcomeLabel = new Label("Welcome", getSkin());
+
+        Label nameLabel = new Label("Name: ", getSkin());
+
+        TextField displayNameTextField = new TextField("", getSkin());
+
+        Label gameListLabel = new Label("Available Games:", getSkin());
+
+        final List<Object> gameList = new List<Object>(getSkin());
+        Object[] games = {
+                "Nate",
+                "Dan",
+                "Tristan",
+                "Doagz",
+                "Nick"
+        };
+        gameList.setItems(games);
+        gameList.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                super.clicked(event, x, y);
+                getGame().setScreen(new LevelScreen(getGame(), gameList.getSelected().toString()));
+            }
+        });
+        ScrollPane scrollPane = new ScrollPane(gameList, getSkin());
+
         Table table = new Table();
-
-        Label welcomeLabel = new Label("Welcome", getGame().getSkin());
-
-        TextField displayNameTextField = new TextField("", getGame().getSkin());
-
-        table.add(welcomeLabel);
+        table.add(welcomeLabel).colspan(2).center();
         table.row();
+        table.add(nameLabel);
         table.add(displayNameTextField);
-        table.setBounds((Gdx.graphics.getWidth() / 2) - 100, Gdx.graphics.getHeight() / 2, 200, 100);
+        table.row();
+        table.add(gameListLabel).colspan(2).center();
+        table.row();
+        table.add(scrollPane).colspan(2).center();
+        table.setBounds((Gdx.graphics.getWidth() / 2) - 100, Gdx.graphics.getHeight() / 2, 200, 200);
 
         getStage().addActor(table);
     }
