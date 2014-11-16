294fe17438c827dd7ecfe70f3e003a4ba87fc52b
tdyer <tdyer@aio-tv.com>
2014-11-16 00:42:02 -0400

    TASK-21: Fix angle calculations

--- core/src/ca/nakednate/game/models/vehicle/Tank.java
+++ core/src/ca/nakednate/game/models/vehicle/Tank.java
@@ -9,13 +9,15 @@ public class Tank extends Vehicle {
 
     private static final int HP = 200;
     private static final int INITIAL_DEPLOYABLE_AMMO = 3;
+    private static final int SPEED = 100;
 
     public Tank() {
-        setTexture(new Texture(Gdx.files.internal("skin/vehicle_jeep_red.png")));
+        setTexture(new Texture(Gdx.files.internal("skin/vehicle_tank_red.png")));
         super.init();
         setHitPoint(HP);
         setWeapon(new Cannon());
         addDeployableWeapon(INITIAL_DEPLOYABLE_AMMO);
+        setSpeed(SPEED);
     }
 
     public void addDeployableWeapon(int quantity) {
